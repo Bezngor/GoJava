f88e6d726ad395f221b9bd525732d4c598146b4a
Vladimir Beznossov <bezngor@gmail.com>
2021-02-04 01:11:28 +0300

    Adding completed work

---
+++ src/main/java/com/Vladimir_Beznossov/javacore/chapter18/HTDemo.java
@@ -0,0 +1,34 @@
+package main.java.com.Vladimir_Beznossov.javacore.chapter18;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class HTDemo {
+    public static void main(String[] args) {
+        Hashtable<String, Double> balance = new Hashtable<>();
+        Enumeration<String> names;
+
+        String str;
+        double bal;
+
+        balance.put("Джон Доу", 3434.34);
+        balance.put("Тoм Смит", 123.22);
+        balance.put("Джейн Бейкер", 1378.00);
+        balance.put("Тод Холл", 99.22);
+        balance.put("Ральф Смит", -19.08);
+
+        System.out.println("показать все счета в хеш-таблице");
+        names = balance.keys();
+
+        while (names.hasMoreElements()) {
+            str = names.nextElement();
+            System.out.println(str + ": " + balance.get(str));
+        }
+        System.out.println();
+
+        // внести 1000 на счет Джона Доу
+        bal = balance.get("Джон Доу");
+        balance.put("Джон Доу", bal + 1000);
+        System.out.println("Новый остаток на счету Джона Доу: " + balance.get("Джон Доу"));
+    }
+}
